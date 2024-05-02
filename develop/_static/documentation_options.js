60161eb849397b1005e439c1f079b221f0f047ba
dhensle <dhensle@users.noreply.github.com>
2024-05-02 21:23:22 +0000

    deploy: e309b849409e9ec391dec885dbbb6f962ac31303

--- develop/_static/documentation_options.js
+++ develop/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.0.5.dev1829+g4085bfc7',
+    VERSION: '1.0.5.dev1834+ge309b849',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
